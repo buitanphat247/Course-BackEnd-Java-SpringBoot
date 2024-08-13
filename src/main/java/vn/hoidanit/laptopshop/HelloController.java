d276f76295a6aeed8d3fd4a9faa70ff45a3e89b5
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-08-13 12:18:12 +0700

    0638

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello world with buitanphat checked!";
+        return "Hello world in home page!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Hello world in user page!";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "Hello world in about page!";
     }
 }
