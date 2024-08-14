12f1f1d5b9cb5b9354ed89c9d077ac537b9a3c18
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-08-14 18:15:25 +0700

    0746

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,28 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+@SuppressWarnings("unused")
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage() {
+        String text = userService.handleHello();
+        System.out.println("text: " + text);
+        return "home.html";
+    }
+
+}
